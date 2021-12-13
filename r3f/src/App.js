8c349359fbde63416daa370267dd8dc72e5535f4
Adisa Narula <adisan19@gmail.com>
2021-12-13 12:48:02 +0700

    drew a box

---
+++ r3f/src/App.js
@@ -0,0 +1,30 @@
+import { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+
+
+function Box(props) {
+  const mesh = useRef();
+  // rotate the box
+  useFrame((state, delta) => {
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+  });
+  // draw the box
+  return (
+    <mesh {...props} ref={mesh}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="#FFAE00" />
+    </mesh>
+  );
+}
+
+function App() {
+  return (
+    <Canvas dpr={window.devicePixelRatio}>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[0, 0, 0]} />
+    </Canvas>
+  );
+}
+
+export default App;
